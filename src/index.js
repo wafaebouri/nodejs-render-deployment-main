74a9b2a96245de353850d6bd767e83654541c294
wafaebouri <116091410+wafaebouri@users.noreply.github.com>
2023-01-26 23:52:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 
 // app.use((req, res) => {
 //   res.send('Hello World!')
