dc814a0a0671a641b1dd08b8d90b69ecafe89251
wafaebouri <116091410+wafaebouri@users.noreply.github.com>
2023-01-27 02:13:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect("mongodb://localhost:27017/mydb", {
+mongoose.connect("mongodb://127.0.0.1:27017/mydb", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
